546ff831181bf001afae712e2ec465fd69c5d8fc
Tiashaadhya005 <tiashaadhya03@gmail.com>
2021-10-27 13:58:35 +0530

    took sample mock fie

--- checknode/models.py
+++ checknode/models.py
@@ -1,23 +1,78 @@
+# there would be 2 types of node :
+# 1)Actor
+# 2)movie
+
+
 from django.db import models
 
 # Create your models here.
 
-class Actor(models.Model):
-    name = models.CharField(max_length=100)
+# class Actor(models.Model):
+#     name = models.CharField(max_length=100)
+
+#     def __str__(self):
+#         return self.name
+
+#     class Meta:
+#         ordering = ('name',)
+
+# class Movie(models.Model):
+#     title = models.CharField(max_length=100)
+#     actors = models.ManyToManyField(Actor)
+#     year = models.IntegerField()
+
+#     def __str__(self):
+#         return self.title
+
+#     class Meta:
+#         ordering = ('title',)
+
+class Genre(models.Model):
+    id=models.IntegerField(primary_key=True)
+    name=models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
-    class Meta:
-        ordering = ('name',)
+class Production_company(models.Model):
+    id=models.IntegerField(primary_key=True)
+    path_logo = models.ImageField()
+    name= models.CharField(max_length=500)
+    origin_country= models.CharField(max_length=100)
 
-class Movie(models.Model):
-    title = models.CharField(max_length=100)
-    actors = models.ManyToManyField(Actor)
-    year = models.IntegerField()
+class Production_countries(models.Model):
+    iso_3166_1 = models.CharField(max_length=100)
+    name = models.CharField(max_length=200)
 
-    def __str__(self):
-        return self.title
+class Spoken_languages(models.Model):
+    english_name = models.CharField(max_length=200)
+    iso_639_1 = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
 
-    class Meta:
-        ordering = ('title',)
+class Mockmovie(models.Model):
+    adult = models.BooleanField()
+    backdrop_path = models.CharField(max_length=500)
+    belongs_to_collection = models.CharField(max_length=500)
+    budget = models.IntegerField()
+    genre= models.ManyToManyField(Genre)
+    homepage=models.CharField(max_length=500)
+    id=models.IntegerField(primary_key=True)
+    imdb_id = models.CharField(max_length=500)
+    original_language = models.CharField(max_length=1000)
+    original_title = models.CharField(max_length=500)
+    overview = models.CharField(max_length=500)
+    popularity = models.FloatField()
+    poster_path = models.CharField(max_length=500)
+    production_companies=models.ManyToManyField(Production_company)
+    production_countries = models.ManyToManyField(Production_countries)
+    release_date = models.CharField(max_length=100)
+    revenue = models.IntegerField()
+    runtime = models.IntegerField() 
+    spoken_languages = models.ManyToManyField(Spoken_languages)
+    status = models.CharField(max_length=500)
+    tagline = models.CharField(max_length=1000)
+    title = models.CharField(max_length=500)
+    video = models.BooleanField()
+    vote_average =models.FloatField()
+    vote_count = models.IntegerField()
+    is_processed = models.BooleanField()
--- checknode/movies.py
+++ checknode/movies.py
@@ -1,6 +1,6 @@
 import graphene
 from graphene_django.types import DjangoObjectType, ObjectType
-from .models import Actor, Movie
+from .models import Actor, Genre, Mockmovie, Movie, Production_company, Production_countries, Spoken_languages
 
 # Create a GraphQL type for the actor model
 class ActorType(DjangoObjectType):
@@ -12,42 +12,73 @@ class MovieType(DjangoObjectType):
     class Meta:
         model = Movie
 
-class Query(ObjectType):
-    # movie properties return one value of ActorType and MovieType respectively, 
-    # and both require an ID that's an integer.
+# class Query1(ObjectType):
+#     # movie properties return one value of ActorType and MovieType respectively, 
+#     # and both require an ID that's an integer.
 
-    #The actors and movies properties return a list of their respective types
+#     #The actors and movies properties return a list of their respective types
     
-    actor = graphene.Field(ActorType, id=graphene.Int())
-    movie = graphene.Field(MovieType, id=graphene.Int())
-    actors = graphene.List(ActorType)
-    movies= graphene.List(MovieType)
+#     actor = graphene.Field(ActorType, id=graphene.Int())
+#     movie = graphene.Field(MovieType, id=graphene.Int())
+#     actors = graphene.List(ActorType)
+#     movies= graphene.List(MovieType)
 
-    def resolve_actor(self, info, **kwargs):
-        '''Functionality to get actor name with particular id'''
-        id = kwargs.get('id')
-        if id is not None:
-            return Actor.objects.get(pk=id)
-        return None
+#     def resolve_actor(self, info, **kwargs):
+#         '''Functionality to get actor name with particular id'''
+#         id = kwargs.get('id')
+#         if id is not None:
+#             return Actor.objects.get(pk=id)
+#         return None
 
-    def resolve_movie(self, info, **kwargs):
+#     def resolve_movie(self, info, **kwargs):
+#         '''Functionality to get movie name with particular id'''
+#         id = kwargs.get('id')
+#         if id is not None:
+#             return Movie.objects.get(pk=id)
+#         return None
+
+#     def resolve_actors(self, info, **kwargs):
+#         '''Functionality to get all actors '''
+#         return Actor.objects.all()
+
+#     def resolve_movies(self, info, **kwargs):
+#         '''Functionality to get all actors '''
+#         return Movie.objects.all()
+
+    # def resolve_node_name(self,info,**kwargs):
+    #     if kwargs.get('type')=='actor':
+    #         return Actor.objects.get(pk=id)
+
+class GenreType(DjangoObjectType):
+    class Meta:
+        model = Genre
+
+class MockMovieType(DjangoObjectType):
+    class Meta:
+        model = Mockmovie
+
+class Production_compType(DjangoObjectType):
+    class Meta:
+        model = Production_company
+
+class Production_countryType(DjangoObjectType):
+    class Meta:
+        model = Production_countries
+
+class SpokenLanguageType(DjangoObjectType):
+    class Meta:
+        model = Spoken_languages
+
+class Query(ObjectType):
+    mock_movies= graphene.Field(MockMovieType, id=graphene.Int())
+
+    def resolve_mock_movie(self, info, **kwargs):
         '''Functionality to get movie name with particular id'''
         id = kwargs.get('id')
         if id is not None:
             return Movie.objects.get(pk=id)
         return None
 
-    def resolve_actors(self, info, **kwargs):
-        '''Functionality to get all actors '''
-        return Actor.objects.all()
-
-    def resolve_movies(self, info, **kwargs):
-        '''Functionality to get all actors '''
-        return Movie.objects.all()
-
-    # def resolve_node_name(self,info,**kwargs):
-    #     if kwargs.get('type')=='actor':
-    #         return Actor.objects.get(pk=id)
 
 schema = graphene.Schema(query=Query)
 
